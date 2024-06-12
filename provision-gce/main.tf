ddc84cf7cb05766c2af5f7a210ce425e58a9ae95
hamonangann <bornyto.hamonangan@ui.ac.id>
2024-06-12 13:55:18 +0700

    split gce master and worker node

--- provision-gce/main.tf
+++ provision-gce/main.tf
@@ -9,8 +9,31 @@ resource "google_compute_firewall" "allow-k3s" {
   target_tags   = ["k3s"]
 }
 
-resource "google_compute_instance" "gce-cluster" {
-  name         = "gce-cluster"
+resource "google_compute_instance" "gce-master-node" {
+  name         = "gce-master-node"
+  zone         = "us-west1-a"
+  machine_type = "e2-standard-4"
+  tags         = ["k3s"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-12"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {}
+  }
+
+  depends_on = [
+    google_compute_firewall.allow-k3s,
+  ]
+}
+
+resource "google_compute_instance" "gce-worker-node" {
+  name         = "gce-worker-node"
   zone         = "us-west1-a"
   machine_type = "n1-highcpu-32"
   tags         = ["k3s"]
@@ -29,6 +52,7 @@ resource "google_compute_instance" "gce-cluster" {
 
   depends_on = [
     google_compute_firewall.allow-k3s,
+    google_compute_instance.gce-master-node,
   ]
 }
 
--- provision-gce/start.sh
+++ provision-gce/start.sh
@@ -14,10 +14,14 @@ terraform apply -var "project=$(gcloud config get-value project)"
 gcloud compute config-ssh
 
 # Instalasi master node dengan K3sup
-CLUSTER_IP=$(gcloud compute instances describe gce-cluster | grep -oP "natIP: \K.*")
+MASTER_IP=$(gcloud compute instances describe gce-master-node | grep -oP "natIP: \K.*")
 curl -sLS https://get.k3sup.dev | sh
 sudo install k3sup /usr/local/bin/
-k3sup install --ip $CLUSTER_IP --context k3s --ssh-key ~/.ssh/google_compute_engine --user $(whoami)
+k3sup install --ip $MASTER_IP --context k3s --ssh-key ~/.ssh/google_compute_engine --user $(whoami)
+
+# Instalasi worker node
+WORKER_IP=$(gcloud compute instances describe gce-worker-node | grep -oP "natIP: \K.*")
+k3sup install --ip $WORKER_IP --server-ip $MASTER_IP --context k3s --ssh-key ~/.ssh/google_compute_engine --user $(whoami)
 
 # Dapatkan kredensial klaster untuk kubectl
 export KUBECONFIG=`pwd`/kubeconfig
@@ -25,6 +29,9 @@ export KUBECONFIG=`pwd`/kubeconfig
 # Salin kredensial klaster ke client
 gcloud compute scp kubeconfig $(whoami)@client:/tmp
 gcloud compute ssh client --command='sudo sh -c "echo export KUBECONFIG=/tmp/kubeconfig >> /etc/profile"'
+
+# Mencegah pod dideploy ke master node
+kubectl taint node gce-master-node node-role.kubernetes.io/master:NoSchedule
 
 # Instalasi load balancer
 kubectl apply -f ../cluster/service.yaml
